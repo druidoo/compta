0e825f6f69252d78c7bba98c36eb7ebf4b9cf4a6
henintsoalice <henintsoalice@gmail.com>
2022-06-22 17:19:06 +0300

    [FIX]remove create on project.project form + adding "Condition" page

--- meritis_account/models/project_project.py
+++ meritis_account/models/project_project.py
@@ -2,13 +2,25 @@
 
 from odoo import models, fields, api, _
 
+class ProjectEmployeeCond(models.Model):
+    _name = 'project.employee_cond.line'
+
+    project_id = fields.Many2one('project.project')
+    employee_id = fields.Many2one('hr.employee')
+    type = fields.Selection([('JF', 'JF'), ('Astreinte', 'Astreinte')])
+    currency_id = fields.Many2one('res.currency', related='employee_id.currency_id')
+    amount = fields.Monetary('Montant', currency_field='currency_id')
+
+
 
 class ProjectProject(models.Model):
     _inherit = 'project.project'
+
     order_id = fields.Many2one('sale.order')
     order_line = fields.One2many(related="order_id.order_line",  string='Order lines', readonly=False)
     order_state = fields.Selection(related="order_id.state")
     amount = fields.Monetary('Montant', compute="_compute_amount")
+    employee_condition = fields.One2many('project.employee_cond.line', 'project_id')
 
     @api.depends('order_line')
     def _compute_amount(self):
@@ -27,3 +39,12 @@ class ProjectProject(models.Model):
 
     def confirm_sale_line(self):
         self.order_id.action_confirm()
+
+    def action_view_project(self):
+        view_id = self.env.ref('project.edit_project').id
+        return {
+            'res_id': self.id,
+            'res_model': self._name,
+            'type': 'ir.actions.act_window',
+            'views': [[view_id, 'form']],
+        }
